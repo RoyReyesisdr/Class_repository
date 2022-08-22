d99601df1d02f0e6b75c4a3afdf06799b52e5866
ejuarezp <edjuarezp@itesm.mx>
2022-08-22 16:49:15 -0500

    consola de js

---
+++ js/intro.js
@@ -0,0 +1,143 @@
+// consola (log, info, warn, error)
+console.log("Hola mundo!");
+console.info("Esto es importante");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error");
+console.assert(1 === 1);
+
+//== compara valores
+console.assert(1 == true);
+
+//=== compara valor y tipo de dato
+console.assert(1 === true);
+
+console.assert(1 == "1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variables, constantes, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert, prompt, confirm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// funciones tradicionales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// funciones modernas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arreglos
+
+//const arreglo = ["Elemento"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//arreglo.push("Otro elemento");
+
+
+
+//arreglo[10] = "Uno más";
+
+
+//arreglos asociativos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.write()
