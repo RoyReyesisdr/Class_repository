d5e21d171567eb0ceca9bb8962009275c47545ed
ejuarezp <edjuarezp@itesm.mx>
2022-08-25 16:00:15 -0500

    aparecer la descripción de html con el evento onclick

--- js/isd.js
+++ js/isd.js
@@ -1,5 +1,6 @@
 let elemento_html = document.getElementById("html");
 
 elemento_html.onclick = () => {
-    console.log("click");
+    let span = document.getElementById("info_html");
+    span.innerHTML = ': Hyper Text Markup Language. Es el lenguaje que utilizan los navegadores web para desplegar las páginas web. Idealmente contienen el contenido y estructura de una página.';
 }
