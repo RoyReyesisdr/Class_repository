5c4c02422f0f65421d99de64c5829c7e500d06d7
ejuarezp <edjuarezp@itesm.mx>
2022-08-25 16:05:05 -0500

    ocultar descripción de html con onclick

--- js/isd.js
+++ js/isd.js
@@ -1,6 +1,15 @@
 let elemento_html = document.getElementById("html");
 
-elemento_html.onclick = () => {
+function descripcion_html() {
     let span = document.getElementById("info_html");
     span.innerHTML = ': Hyper Text Markup Language. Es el lenguaje que utilizan los navegadores web para desplegar las páginas web. Idealmente contienen el contenido y estructura de una página.';
+    elemento_html.onclick = ocultar_descripcion_html;
 }
+
+function ocultar_descripcion_html() {
+    let span = document.getElementById("info_html");
+    span.innerHTML = '';
+    elemento_html.onclick = descripcion_html;
+}
+
+elemento_html.onclick = descripcion_html;
