bc1a5dc7abcb36e5bd5cdd1b86f600452a40b2ec
ejuarezp <edjuarezp@itesm.mx>
2022-08-25 16:48:20 -0500

    aparecer y desaparecer imágenes con eventos del mouse

--- js/isd.js
+++ js/isd.js
@@ -37,3 +37,21 @@ porcentaje_descuento.onkeyup = () => {
         document.getElementById("promocion").innerHTML = "Querías hacer trampa... ¡Perdiste tu descuento!";
     }
 }
+
+let label_cafe = document.getElementById("label_cafe");
+label_cafe.onmouseover = () => {
+    document.getElementById("imagen_cafe").innerHTML = '<img src="https://as01.epimg.net/deporteyvida/imagenes/2018/06/19/portada/1529402043_039778_1529402207_noticia_normal_recorte1.jpg" alt="Imagen de café" width="400">';
+}
+
+label_cafe.onmouseleave = () => {
+    document.getElementById("imagen_cafe").innerHTML = '';
+}
+
+let label_cerveza = document.getElementById("label_cerveza");
+label_cerveza.onmouseover = () => {
+    document.getElementById("imagen_cerveza").innerHTML = '<img src="https://pbs.twimg.com/profile_images/1526257909033709570/99dbdXRh_400x400.jpg" alt="Imagen de cerveza" width="400">';
+}
+
+label_cerveza.onmouseleave = () => {
+    document.getElementById("imagen_cerveza").innerHTML = '';
+}
