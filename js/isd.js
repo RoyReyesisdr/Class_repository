299e8847aac2f1d6ccc31d382b07b274b555b53b
ejuarezp <edjuarezp@itesm.mx>
2022-08-29 16:51:21 -0500

    componentes de materializecss

--- js/isd.js
+++ js/isd.js
@@ -1,19 +1,3 @@
-let elemento_html = document.getElementById("html");
-
-function descripcion_html() {
-    let span = document.getElementById("info_html");
-    span.innerHTML = ': Hyper Text Markup Language. Es el lenguaje que utilizan los navegadores web para desplegar las páginas web. Idealmente contienen el contenido y estructura de una página.';
-    elemento_html.onclick = ocultar_descripcion_html;
-}
-
-function ocultar_descripcion_html() {
-    let span = document.getElementById("info_html");
-    span.innerHTML = '';
-    elemento_html.onclick = descripcion_html;
-}
-
-elemento_html.onclick = descripcion_html;
-
 let descuento = document.getElementById("descuento");
 descuento.onclick = () => {
     let porcentaje_descuento = document.getElementById("porcentaje_descuento").value;
