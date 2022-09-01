d28ec50a7c6db7ff15d1384725e8b1364b941528
ejuarezp <edjuarezp@itesm.mx>
2022-09-01 16:11:09 -0500

    escribir archivos con node

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// const filesystem = require('fs');
-// filesystem.writeFileSync('hola.txt', 'Hola desde node');
+const filesystem = require('fs');
+filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+
 // console.log("hola desde node");
+
+
+
+
 
 
 
