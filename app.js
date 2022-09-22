0fa8c11ec624010ad4b721bbc555d8d2347e4c62
ejuarezp <edjuarezp@itesm.mx>
2022-09-22 16:17:31 -0500

    poner en el lugar correcto las configuraciones de app

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
